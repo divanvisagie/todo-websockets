4b74c9cf7aaf1088c3cac2b09366406d62a3cede
Divan Visagie <visagiedivan@gmail.com>
2017-06-02 14:34:09 +0200

    serve files from akka

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,10 +2,13 @@ package example
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Source}
+import akka.http.scaladsl.model.StatusCodes
+
 
 object Hello extends App {
   implicit val system = ActorSystem()
@@ -17,14 +20,30 @@ object Hello extends App {
         case tm: TextMessage => TextMessage(Source.single("Hello ") ++ tm.textStream)
       }
 
+  val staticResources =
+    pathPrefix("") {
+//        pathEnd {
+//          redirect("/api/swagger/", StatusCodes.TemporaryRedirect)
+//        } ~
+        pathSingleSlash {
+          getFromDirectory("web_client/build/default/index.html")
+        } ~
+        getFromDirectory("web_client/build/default")
+    }
   val route =
+    staticResources ~
+    path("ping") {
+      get {
+        complete("pong")
+      }
+    } ~
     path ("todo") {
       get {
         handleWebSocketMessages(greeterWebSocketService)
       }
     }
 
-  val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080);
+  val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
   println("Hello World")
 }
