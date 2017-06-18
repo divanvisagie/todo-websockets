618d79bd06eeae0b8e44eafc271584b3d868bc43
Divan Visagie <visagiedivan@gmail.com>
2017-06-18 15:07:17 +0200

    remove unused test

--- src/test/scala/example/TodoAppSpec.scala
+++ src/test/scala/example/TodoAppSpec.scala
@@ -10,20 +10,3 @@ class TodoAppSpec extends FlatSpec with Matchers {
 }
 
 
-//class SubscriptionHandlerSpec extends FlatSpec with Matchers {
-//  "Parsing 'SUBSCRIBE:todos'" should
-//    "return action==SUBSCRIBE and collection==todos and data==''" in {
-//    val messageMetadata: MessageMetadata = SubscriptionHandler.parseSubscriberMessageMetadata("SUBSCRIBE:todos")
-//    messageMetadata.action     shouldEqual "SUBSCRIBE"
-//    messageMetadata.collection shouldEqual "todos"
-//    messageMetadata.data       shouldEqual ""
-//  }
-//
-//  "parsing 'UPDATE: TODOS => data'" should
-//    "return action==UPDATE and collection==todos and data==data" in {
-//    val messageMetadata = SubscriptionHandler.parseSubscriberMessageMetadata("UPDATE:todos => data")
-//    messageMetadata.action     shouldEqual "UPDATE"
-//    messageMetadata.collection shouldEqual "todos"
-//    messageMetadata.data       shouldEqual "data"
-//  }
-//}
