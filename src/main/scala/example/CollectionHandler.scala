e25b1961b667b013e1631c06eb640bec00344a99
Divan Visagie <visagiedivan@gmail.com>
2017-06-19 11:03:49 +0200

    update now done in mongo

--- src/main/scala/example/CollectionHandler.scala
+++ src/main/scala/example/CollectionHandler.scala
@@ -4,11 +4,18 @@ import akka.actor.{Actor, ActorRef, Terminated}
 import akka.event.Logging
 import com.mongodb.DBObject
 import com.mongodb.casbah.MongoClient
+import com.mongodb.casbah.commons.MongoDBObject
+import com.mongodb.casbah.commons.conversions.scala._
+import com.mongodb.casbah.WriteConcern
+import com.mongodb.casbah.Imports._
+import com.mongodb.CommandResult
 import io.circe._
 import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
 import io.circe.generic.semiauto._
+
+import org.bson.types.ObjectId
 
 import scala.collection.mutable
 
@@ -16,8 +23,17 @@ case class Todo(uid: String, text: String)
 case class NewTodo(text: String)
 case class DeleteTodo(uid: String)
 
+
+
 class CollectionHandler(collectionName: String) extends Actor {
   import CollectionHandler._
+
+  implicit val encodeIntOrString: Encoder[Either[Int, String]] =
+    Encoder.instance(_.fold(_.asJson, _.asJson))
+
+  implicit val decodeIntOrString: Decoder[Either[Int, String]] =
+    Decoder[Int].map(Left(_)).or(Decoder[String].map(Right(_)))
+
   val log = Logging(context.system, this)
   var subscribers: Set[ActorRef] = Set.empty
   var todos: mutable.Map[String,Todo] = mutable.Map[String,Todo]()
@@ -27,10 +43,7 @@ class CollectionHandler(collectionName: String) extends Actor {
   def uuid: String = java.util.UUID.randomUUID.toString
 
   def updateEveryone(): Unit = {
-
-
-
-    val jsonString = todos.values.asJson.toString()
+    val jsonString = collectionValues.asJson.toString()
     val count = todos.values.toList.length
     log.info(s"updating all $count subscribers with $jsonString")
     subscribers.foreach { subscriber =>
@@ -38,35 +51,30 @@ class CollectionHandler(collectionName: String) extends Actor {
     }
   }
 
+  val collection = mongoClient("local")(collectionName)
+
+  def collectionValues: Array[Map[String,String]] = {
+
+    val collectionItems = collection.find()
+    collectionItems.map { item =>
+      val uid = item.get("_id").toString
+      val map = mutable.Map[String,String](
+        "uid" -> uid
+      )
+      item.keySet().forEach { key =>
+        val value = item.get(key).toString
+        if (key != "_id") map += (key -> value)
+      }
+      map.toMap
+    }.toArray
+  }
+
   def receive: Receive = {
     case Subscribe(subscriber: ActorRef) =>
       log.info(s"new subscriber to $collectionName")
       subscribers += subscriber
       context.watch(subscriber)
-
-      val collection = mongoClient("local")(collectionName)
-      val collectionItems = collection.find()
-      println(s"--->>>> $collectionItems")
-
-      implicit val encodeIntOrString: Encoder[Either[Int, String]] =
-        Encoder.instance(_.fold(_.asJson, _.asJson))
-
-      implicit val decodeIntOrString: Decoder[Either[Int, String]] =
-        Decoder[Int].map(Left(_)).or(Decoder[String].map(Right(_)))
-
-      val collectionMap = collectionItems.map { item =>
-         val uid = item.get("_id").toString
-         val map = mutable.Map[String,String](
-           "uid" -> uid
-         )
-         item.keySet().forEach { key =>
-           val value = item.get(key).toString
-           if (key != "_id") map += (key -> value)
-         }
-         map.toMap
-      }
-
-      val jsonString = collectionMap.toArray.asJson.toString()
+      val jsonString = collectionValues.asJson.toString()
       subscriber ! Subscriber.OutgoingMessage(jsonString)
 
     case Terminated(subscriber) =>
@@ -75,8 +83,10 @@ class CollectionHandler(collectionName: String) extends Actor {
     case AddMessage(data) =>
       decode[NewTodo](data) match {
         case Right(todo) =>
-          val uid: String = uuid
-          todos += (uid -> Todo(uid,todo.text))
+          collection.insert(MongoDBObject(
+            "text" -> todo.text,
+            "uid" -> uuid
+          ))
           updateEveryone()
         case Left(error) =>
           log.info(s"Unable to parse new todo: $error")
@@ -85,7 +95,9 @@ class CollectionHandler(collectionName: String) extends Actor {
     case UpdateMessage(data) =>
       decode[Todo](data) match {
         case Right(todo) =>
-          todos(todo.uid) = todo
+          val query = MongoDBObject("uid" -> todo.uid)
+          val update = $set("text" -> todo.text)
+          collection.update(query, update)
           updateEveryone()
         case Left(error) =>
           log.info(s"Unable to parse todo: $error")
@@ -101,10 +113,6 @@ class CollectionHandler(collectionName: String) extends Actor {
       }
   }
 
-  val db = mongoClient("local")
-  for (name <- db.collectionNames()) {
-    println(s"Name: $name")
-  }
 }
 object CollectionHandler {
   case class Subscribe(subscriber: ActorRef)
